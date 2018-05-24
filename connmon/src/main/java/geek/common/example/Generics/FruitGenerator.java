5a3dd76910c52766b4392e8c2067c2fb4bd7ce97
dujianjun <1208jun1013>
2018-05-24 13:24:27 +0800

    2018-5-24

---
+++ connmon/src/main/java/geek/common/example/Generics/FruitGenerator.java
@@ -0,0 +1,15 @@
+package geek.common.example.Generics;
+
+/**
+ * Description:
+ * Program Name:
+ * author :dujianjun
+ * Date:2018-05-24  10:40  星期四
+ * version 1.0
+ */
+public class FruitGenerator<T> implements Generator<T> {
+    @Override
+    public T next() {
+        return null;
+    }
+}
